a16bfb2c6b38d977a09a3b5461b842b335b3d0a3
Jonathan Siler <jonjonsiler@hotmail.com>
2019-09-04 08:39:16 -0500

    Add delete and update directives.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ItemService } from '../../services/item.service';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,11 +14,49 @@ export class HomePage implements OnInit {
 
   constructor(
     private router: Router,
-    public itemService: ItemService
+    public itemService: ItemService,
+    public navCtrl: NavController, 
+    public alertCtrl: AlertController
   ){}
 
   ngOnInit(){
     this.items = this.itemService.getItems();
   }
 
+  deleteItem(index) {
+    this.items.splice(index, 1);
+  }
+
+  async updateItem(id) {
+    let item = this.items[this.items.findIndex(item => item.id === id)];
+    const alert = await this.alertCtrl.create({
+      header: 'Update Item?',
+      message: 'Type in your new Item information to update.',
+      inputs: [
+        { 
+          name: 'title',
+          placeholder: 'Item Name',
+          value: item.title
+        },
+        {
+          name: "description",
+          placeholder: "Item Description",
+          value: item.description
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel' 
+        },
+        { 
+          text: 'Update', 
+          handler: data => { 
+            this.itemService.updateItem(Object.assign(data, {id: id}));
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
